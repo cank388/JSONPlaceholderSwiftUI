7baaa42e9c052c455de61939c98823fbcdbbe49e
Can Kalender <cankalender.tr@gmail.com>
2025-01-28 21:06:36 +0300

    Initial Commit

---
+++ JSONPlaceholderSwiftUI/JSONPlaceholderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONPlaceholderSwiftUIApp.swift
+//  JSONPlaceholderSwiftUI
+//
+//  Created by Can Kalender on 28.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct JSONPlaceholderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
